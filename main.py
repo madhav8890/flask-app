1c85c9ea7e76755955dac55e69267c4cb23a330f
Madhav Prajapati <75035012+madhav8890@users.noreply.github.com>
2024-08-07 11:07:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def main():
-    return "Hello From madhav"
+    return "Hello From madhav test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
